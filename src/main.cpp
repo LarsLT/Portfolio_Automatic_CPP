6d99bcb6901bae954205205417d7541f53bb4d3f
LarsLT <lars.tokkie@student.hu.nl>
2025-03-20 19:33:50 +0100

    void(main): way to choose what to do

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,79 @@
 #include "Portfolio.hh"
 #include "Input.hh"
 #include <iostream>
+#include <string>
+#include <format>
+
+Path storage_path()
+{
+    Input::clear_console();
+    std::string path = "C:/Projects/zelf/C++/Portfolio_Automatic_C--/storage/storage.txt";
+
+    if (Input::get_yes_no(std::format("Klopt dit path\n{}\n", path)))
+    {
+        Input::clear_console();
+        return Path(path);
+    }
+    else
+    {
+        Input::clear_console();
+        return Path();
+    }
+}
 
 int main()
 {
+    Input::clear_console();
+
+    Path path = storage_path();
+
     while (true)
     {
-        Portfolio portfolio("C:/Projects/zelf/C++/Portfolio_Automatic_C--/storage/storage.txt");
+        std::cout << "Kies een functie om uit te voeren:\n";
+        std::cout << "1. tabel toevoegen\n";
+        std::cout << "2. portfolio path updaten\n";
+        std::cout << "3. Algemeen updaten\n";
+        std::cout << "4. Exit\n";
 
-        std::string table = Table::make_everything();
+        std::string wat = Input::get_input("");
 
-        portfolio.add_item(table, std::nullopt);
+        switch (std::stoi(wat))
+        {
+        case 1:
+        {
+            Portfolio portfolio(path); // TODO dont like creating an object for bs
+            std::string table = Table::make_everything();
+
+            portfolio.add_item(table, std::nullopt);
+            Input::clear_console();
+            break;
+        }
+        case 2:
+        {
+            path.update_path(path.PORTFOLIO);
+            Input::clear_console();
+            break;
+        }
+        case 3:
+        {
+            Portfolio portfolio(path); // TODO dont like creating an object for bs
+
+            portfolio.update_algemeen();
+
+            Input::clear_console();
+            break;
+        }
+        case 4:
+        {
+            return 0;
+            Input::clear_console();
+            break;
+        }
+
+        default:
+        {
+            break;
+        }
+        }
     }
 }
