87c7f2193fdee20626dbe9c55ab201fc821a5937
LarsLT <lars.tokkie@student.hu.nl>
2025-03-22 21:05:08 +0100

    fix: order updated the order of things

--- src/main.cpp
+++ src/main.cpp
@@ -29,12 +29,14 @@ int main()
 
     Path path = storage_path();
 
+    Portfolio portfolio(path);
+
     while (true)
     {
         std::cout << "Kies een functie om uit te voeren:\n";
         std::cout << "1. tabel toevoegen\n";
-        std::cout << "2. portfolio path updaten\n";
-        std::cout << "3. Algemeen updaten\n";
+        std::cout << "2. Algemeen updaten\n";
+        std::cout << "3. portfolio path updaten\n";
         std::cout << "4. Exit\n";
 
         std::string wat = Input::get_input("");
@@ -43,7 +45,6 @@ int main()
         {
         case 1:
         {
-            Portfolio portfolio(path); // TODO dont like creating an object for bs
             std::string table = Table::make_everything();
 
             portfolio.add_item(table, std::nullopt);
@@ -52,28 +53,29 @@ int main()
         }
         case 2:
         {
-            path.update_path(path.PORTFOLIO);
-            Input::clear_console();
-            break;
-        }
-        case 3:
-        {
-            Portfolio portfolio(path); // TODO dont like creating an object for bs
 
             portfolio.update_algemeen();
 
             Input::clear_console();
             break;
         }
+        case 3:
+        {
+            path.update_path(path.PORTFOLIO);
+            Input::clear_console();
+            portfolio = Portfolio(path);
+            break;
+        }
         case 4:
         {
-            return 0;
             Input::clear_console();
+            return 0;
             break;
         }
 
         default:
         {
+            std::cout << "Kies een geldige optie\n";
             break;
         }
         }
